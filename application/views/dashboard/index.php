22d8386065174a0bd9b42619eb90393392716ed7
Fahry Lafir <fahrydgt@gmail.com>
2019-04-20 01:10:49 +0530

    dashausha

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -139,7 +139,7 @@
           <!-- MAP & BOX PANE -->
           <div class="box box-success">
             <div class="box-header with-border">
-              <h3 class="box-title">Visitors Report</h3>
+              <h3 class="box-title">Location Stock Summary</h3>
 
               <div class="box-tools pull-right">
                 <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
