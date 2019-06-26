b1863105641967ab981826d0b2dcd8f507f2d3eb
Fahry Lafir <fahrydgt@gmail.com>
2019-06-26 11:09:13 +0530

    hihjhkj

---
+++ application/libraries/DomPDFgen.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once dirname(__FILE__) . '/dompdf/autoload.inc.php';
+
+// Reference the Dompdf namespace 
+
+            use Dompdf\Dompdf; 
+            
+class DomPDFgen extends Dompdf{
+    
+}
